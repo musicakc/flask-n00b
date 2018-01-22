693078514593f9ef4c09607f74e2b24e7f246592
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2018-01-22 20:34:58 +0530

    flask error

--- blog/app/app.py
+++ blog/app/app.py
@@ -27,3 +27,52 @@ flask_db = FlaskDB(app)
 database = flask.db.database
 
 oembed.providers = bootstrap_basic(OEmbedCache())
+
+class Entry(flask_db.Model):
+	title = CharField()
+	slug = CharField(unique=True)
+	content = TextField()
+	published = BooleanField(index=True)
+	timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = re.sub('[^\w]+', '-', self.title.lower())
+		ret = super(Entry, self).save(*args, **kwargs)
+		self.update_search_index()
+		return ret
+	def update_search_index(self):
+		try:
+			fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+		except FTSEntry.DoesNotExist:
+			fts_entry = FTSEntry(entry_id = self.id)
+			force_insert = True
+		else:
+			force_insert = False
+		fts_entry.content = '\n'.join((self.title, self.content))
+		fts_entry.save(force_insert=force_insert)
+
+class FTSEntry(FTSModel):
+	entry_id = IntegerField()
+	content = TextField()
+
+	class Meta:
+		database = database
+
+@app.template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+	querystring = dict((key, value) for key, value in request_args.items())
+	for key in keys_to_remove:
+		querystring.pop(key, None)
+	querystring.update(new_values)
+	return urllib.urlencode(querystring)
+@app.errorhandler(404)
+def not_found(exc):
+	return Response('<h3> Not Found </h3'), 404
+
+def main():
+	database.create_tables([Entry, FTSEntry], safe=True)
+	app.run(debug=True)
+
+if __name__ == '__main__':
+	main()
